483b45f180c5624664762fcb9e4b06f6af999dc5
iossocket <avx302@gmail.com>
2019-09-22 21:32:33 +0800

    add class scanner to get all the Classes by package name

--- framework/src/main/java/com/iossocket/starter/MiniApplication.java
+++ framework/src/main/java/com/iossocket/starter/MiniApplication.java
@@ -1,7 +1,10 @@
 package com.iossocket.starter;
 
+import com.iossocket.core.ClassScanner;
 import com.iossocket.web.server.TomcatServer;
 import org.apache.catalina.LifecycleException;
+
+import java.util.List;
 
 public class MiniApplication {
     public static void run(Class<?> cls, String[] args) {
@@ -9,7 +12,9 @@ public class MiniApplication {
         TomcatServer tomcatServer = new TomcatServer(args);
         try {
             tomcatServer.startServer();
-        } catch (LifecycleException e) {
+            List<Class<?>> classList = ClassScanner.scanClasses(cls.getPackage().getName());
+            classList.forEach(it -> System.out.println(it.getName()));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
