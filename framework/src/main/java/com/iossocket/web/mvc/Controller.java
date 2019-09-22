edfd23436200be328bdebaa3493dc55038685ba8
iossocket <avx302@gmail.com>
2019-09-22 18:24:01 +0800

    rename test moduel to app module, and add anotations

---
+++ framework/src/main/java/com/iossocket/web/mvc/Controller.java
@@ -0,0 +1,9 @@
+package com.iossocket.web.mvc;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Controller {
+}
---
+++ framework/src/main/java/com/iossocket/web/mvc/RequestParam.java
@@ -0,0 +1,9 @@
+package com.iossocket.web.mvc;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestParam {
+}
